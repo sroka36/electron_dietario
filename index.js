a9e6bd0057b72b4a8c03939b6e659429ec1721f3
sroka36 <wugpie@gmail.com>
2025-12-28 23:34:04 +0900

    Add plus function

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ for (let i = 0; i < number.length; i++) {
     number[i].textContent = parseInt(number[i].textContent).toLocaleString("ko-KR")
 }
 
+function plusDietario(){
+    const tbody = document.querySelector(".dietario")
+    const newRow = document.createElement("tr")
+    newRow.innerHTML = `
+        <td>2025-12-27</td>
+        <td>.</td>
+        <td>.</td>
+        <td class="number">3000</td>
+    `
+    tbody.appendChild(newRow)
+}
